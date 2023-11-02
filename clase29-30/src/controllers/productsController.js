5c26de6fa198ec19ff611b57db3bcfce99663d92
Andrey Bejarano <andreybejarano@gmail.com>
2023-11-02 02:28:45 +0200

    clase30

--- clase29-30/src/controllers/productsController.js
+++ clase29-30/src/controllers/productsController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -23,25 +23,48 @@ const controller = {
 	// Create - Form to create
 	create: (req, res) => {
 		// Do the magic
+		res.render('product-create-form');
 	},
 
 	// Create -  Method to store
 	store: (req, res) => {
 		// Do the magic
+		const newProduct = {
+			id: products[products.length - 1].id + 1,
+			...req.body,
+			image: req.file?.filename || "default-image.png"
+		};
+		products.push(newProduct);
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+		res.redirect('/products');
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
 		// Do the magic
+		const product = products.find((product) => product.id == req.params.id);
+		res.render('product-edit-form', { productToEdit: product });
 	},
 	// Update - Method to update
 	update: (req, res) => {
 		// Do the magic
+		const indexProduct = products.findIndex((product) => product.id == req.params.id);
+		products[indexProduct] = {
+			...products[indexProduct],
+			...req.body
+		};
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+		res.redirect('/products');
 	},
 
 	// Delete - Delete one product from DB
 	destroy: (req, res) => {
 		// Do the magic
+		// products = products.filter((product) => product.id != req.params.id);
+		const indexProduct = products.findIndex((product) => product.id == req.params.id);
+		products.splice(indexProduct, 1);
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+		res.redirect('/products');
 	}
 };
 
--- clase29-30/src/routes/products.js
+++ clase29-30/src/routes/products.js
@@ -1,28 +1,42 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../../public/images/products'));
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const filename = `${Date.now()}-product${ext}`;
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage: storage })
 
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
 
-/*** GET ALL PRODUCTS ***/ 
-router.get('/', productsController.index); 
+/*** GET ALL PRODUCTS ***/
+router.get('/', productsController.index);
 
-/*** CREATE ONE PRODUCT ***/ 
-// router.???('/???/', productsController.create); 
-// router.???('/', productsController.store); 
-
-
-/*** GET ONE PRODUCT ***/ 
-router.get('/detail/:id', productsController.detail); 
-
-/*** EDIT ONE PRODUCT ***/ 
-// router.???('/:id/???', productsController.edit); 
-// router.???('/:id', productsController.update); 
+/*** CREATE ONE PRODUCT ***/
+router.get('/create', productsController.create);
+router.post('/create', upload.single('image'), productsController.store);
 
 
-/*** DELETE ONE PRODUCT***/ 
-// router.???('/:id', productsController.destroy); 
+/*** GET ONE PRODUCT ***/
+router.get('/detail/:id', productsController.detail);
+
+/*** EDIT ONE PRODUCT ***/
+router.get('/:id/edit', productsController.edit); 
+router.put('/:id/edit', productsController.update);
+
+/*** DELETE ONE PRODUCT***/
+router.delete('/:id/delete', productsController.destroy);
 
 
 module.exports = router;
