faf765b5a55df89a992beeb670f49e5d6e7a7429
Andrey Bejarano <andreybejarano@gmail.com>
2024-02-07 21:07:26 -0300

    clase54

---
+++ clase54/src/App.jsx
@@ -0,0 +1,22 @@
+import SideBar from './components/SideBar'
+import ContentWrapper from './components/ContentWrapper'
+
+import './App.css'
+
+function App() {
+  return (
+    <div id="wrapper">
+
+      {/* <!-- Sidebar --> */}
+      <SideBar />
+      {/* <!-- End of Sidebar --> */}
+
+      {/* <!-- Content Wrapper --> */}
+      <ContentWrapper />
+      {/* <!-- End of Content Wrapper --> */}
+
+    </div>
+  )
+}
+
+export default App
